5f88f73a03d7d15224a57cd037a90b29ca348d1e
Fabelixio <felixsewellknight@gmail.com>
2023-08-12 14:57:01 +0100

    create functional single game

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rock-paper-scissors
 Odin project JavaScript practice project
+This project aims to demonstrate and practice basic competency of the JavaScript fundamentals used outside of a learning environment.
--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-let choice;
+let choice = 'paper';
 let t = 0
 let w = 0
 let l = 0
+let invalid = 0
 function getComputerChoice() {
 var compChoice = Math.floor(Math.random()*100)
 console.log(compChoice)
@@ -14,8 +15,7 @@ if(compChoice <= 33) {
 }
 }
 
-function playRound() {
-    let computerSelection = getComputerChoice()
+function playRound(choice, computerSelection) {
     if(choice.toLowerCase() == 'rock' && computerSelection == 'rock') {
         t++
         return 'Computer: rock! It\'s a tie!'
@@ -27,5 +27,26 @@ function playRound() {
         return 'Computer: scissors! It\'s a tie!'
     } else if(choice.toLowerCase() == 'rock' && computerSelection == 'paper') {
         l++
+        return 'Computer: paper! You lose!'
+    } else if(choice.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+        l++
+        return 'Computer: scissors! You lose!'
+    } else if(choice.toLowerCase() == 'scissors' && computerSelection == 'rock') {
+        l++
+        return 'Computer: rock! You lose!'
+    } else if(choice.toLowerCase() == 'rock' && computerSelection == 'scissors') {
+        w++
+        return 'Computer: scissors! You win!'
+    } else if(choice.toLowerCase() == 'paper' && computerSelection == 'rock') {
+        w++
+        return 'Computer: rock! You win!'
+    } else if(choice.toLowerCase() == 'scissors' && computerSelection == 'paper') {
+        w++
+        return 'Computer: paper! You win!'
+    } else {
+        invalid++
+        return 'Please choose rock paper or scissors!'
     }
 }
+const computerSelection = getComputerChoice()
+console.log(playRound(choice, computerSelection))
