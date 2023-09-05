824a1aa2c279b157aaf8fc6886762aaef0413465
Fabelixio <felixsewellknight@gmail.com>
2023-09-05 14:20:00 +0100

    rewrite round functionality and add scoreboard

--- javascript.js
+++ javascript.js
@@ -1,8 +1,7 @@
 let choice;
-let t = 0
 let w = 0
 let l = 0
-let invalid = 0
+const buttons = document.querySelectorAll('input')
 function getComputerChoice() {
 var compChoice = Math.floor(Math.random()*100)
 console.log(compChoice)
@@ -14,51 +13,56 @@ if(compChoice <= 33) {
     return 'scissors'
 }
 }
-let computerSelection = getComputerChoice()
-function playRoundRock(computerSelection) {
-    if(computerSelection === 'rock') {
-        t++
-        return 'Computer: rock! It\'s a tie!'
-    } else if(computerSelection == 'paper') {
-        l++
-        return 'Computer: paper! You lose!'
-    } else if(computerSelection == 'scissors') {
+function endGame () {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function playRound(playerSelection) {
+    let result = ''
+    let computerSelection = getComputerChoice()
+
+    if(playerSelection === computerSelection) {
+        result = ('Computer: ' + playerSelection + '! It\'s a tie!' + '<br>Player Score: '
+        + w + '<br>Computer Score: ' + l)
+
+    } else if((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
         w++
-        return 'Computer: scissors! You win!'
-    }
-}
+        result = ('Computer: ' + computerSelection + '! You Win!' + '<br>Player Score: '
+        + w + '<br>Computer Score: ' + l)
+        
+        if (w == 5) {
+            result += '<br><br>Victory is yours! Reload the page to play again.'
+            endGame()
+        }
 
-function playRoundScissors(computerSelection) {
-    if(computerSelection === 'scissors') {
-    t++
-    return 'Computer: scissors! It\'s a tie!'
-    } else if(computerSelection == 'rock') {
-    l++
-    return 'Computer: rock! You lose!'
-    } else if(computerSelection == 'paper') {
-    w++
-    return 'Computer: paper! You win!'
-    }
-}
-
-function playRoundPaper(computerSelection) {
-    if(computerSelection === 'paper') {
-        t++
-        return 'Computer: paper! It\'s a tie!'
-    } else if(computerSelection == 'scissors') {
+    } else {
         l++
-        return 'Computer: scissors! You lose!'
-    } else if(computerSelection == 'rock') {
-        w++
-        return 'Computer: rock! You win!'
+        result = ('Computer: ' + computerSelection + '! You Lose!' + '<br>Player Score: '
+        + w + '<br>Computer Score: ' + l)
+
+        if(l == 5) {
+            result += '<br><br>Computer wins the Game! Better luck next time! Reload the page to try again.'
+            endGame()
+        }
     }
+
+    document.getElementById('scoreBoard').innerHTML = result
+    return
 }
-
-
-
 
 //dom ui
-const rockbtn = document.querySelector('#rock')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        playRound(button.value)
+    })
+})
+
+/*const rockbtn = document.querySelector('#rock')
 rockbtn.addEventListener('click', playRoundRock)
 const paperbtn = document.querySelector('#paper')
 paperbtn.addEventListener('click', playRoundPaper)
